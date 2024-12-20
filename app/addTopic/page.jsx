5792777d595466e5efca05af794c8baf1fc1ebd6
ICELINK <icelink02@gmail.com>
2024-12-20 12:31:28 +0500

    add base url

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -18,7 +18,7 @@ export default function AddTopic() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/topics", {
+      const res = await fetch(`${process.env.BASE_URL}/api/topic`, {
         method: "POST",
         headers: {
           "Content-type": "application/json",
